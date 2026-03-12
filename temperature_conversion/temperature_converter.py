a48afa1c7b75abc0cc5c4b4e947b2d9043da5cd8
s-royy <sayanroy7009@gmail.com>
2026-03-12 22:06:03 +0530

    Add temperature conversion functions for Celsius, Fahrenheit, and Kelvin

--- temperature_conversion/temperature_converter.py
+++ temperature_conversion/temperature_converter.py
@@ -1,1 +1,22 @@
+def celsius_to_fahrenheit(celsius: float) -> float:
+    return (celsius * 9 / 5) + 32
 
+
+def celsius_to_kelvin(celsius: float) -> float:
+    return celsius + 273.15
+
+
+def fahrenheit_to_celsius(fahrenheit: float) -> float:
+    return (fahrenheit - 32) * 5 / 9
+
+
+def fahrenheit_to_kelvin(fahrenheit: float) -> float:
+    return celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit))
+
+
+def kelvin_to_celsius(kelvin: float) -> float:
+    return kelvin - 273.15
+
+
+def kelvin_to_fahrenheit(kelvin: float) -> float:
+    return celsius_to_fahrenheit(kelvin_to_celsius(kelvin))

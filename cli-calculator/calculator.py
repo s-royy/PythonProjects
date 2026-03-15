a6c879596fb3ef9275cb8572ad98b0873f9b1016
s-royy <sayanroy7009@gmail.com>
2026-03-15 23:38:22 +0530

    Update calculator.py

--- cli-calculator/calculator.py
+++ cli-calculator/calculator.py
@@ -1,1 +1,22 @@
+def add(a: float, b: float) -> float:
+    return a + b
 
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    return a / b
+
+
+def get_float(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Please enter a valid number.")

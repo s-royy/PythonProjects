63d0836d17b83efa921ac9ed021532465bbb8506
s-royy <sayanroy7009@gmail.com>
2026-03-19 23:29:56 +0530

    Update password-generator.py

--- password_generator/password-generator.py
+++ password_generator/password-generator.py
@@ -1,1 +1,12 @@
+import random
+import string
+
+
+def generate_password(length: int, use_digits: bool, use_symbols: bool) -> str:
+    characters = string.ascii_letters
+    if use_digits:
+        characters += string.digits
+    if use_symbols:
+        characters += string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
 
